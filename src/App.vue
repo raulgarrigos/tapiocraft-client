1aa5573474da5387c5e4800ab19179fbf9941d12
raulgp <raul.garrigos@hotmail.com>
2024-03-07 12:22:41 +0100

    added jsconfig, App.vue and main.js

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <h1>TapioCraft</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: Arial, sans-serif;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import App from "./App.vue";
+
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+  routes: [
+    // { path: "/", component: Boards },
+  ],
+});
+
+new Vue({
+  router,
+  render: (h) => h(App),
+}).$mount("#root");
