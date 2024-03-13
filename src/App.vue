fa9bb1ddb6fa0b2d803f4efe9907d1bfb9665730
raulgp <raul.garrigos@hotmail.com>
2024-03-13 13:03:58 +0100

    auth context

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,22 @@
 </template>
 
 <script>
+import { onMounted } from "vue";
+import authContext from "./context/auth.context.js";
+
 export default {
-  name: 'App'
-}
+  name: "App",
+  setup() {
+    // Cuando el componente se monta, se llama a authenticateUser para verificar la autenticación
+    onMounted(() => {
+      authContext.authenticateUser();
+    });
+
+    return {
+      authContext, // Hace que el contexto de autenticación esté disponible en el template
+    };
+  },
+};
 </script>
 
 <style>
--- src/context/auth.context.js
+++ src/context/auth.context.js
@@ -1,31 +1,23 @@
-import { createContext, ref, onMounted } from "vue";
+import { ref, onMounted } from "vue";
 import service from "../services/config";
 
-const AuthContext = createContext();
-
-function useAuth() {
-  const isLoggedIn = ref(false);
-  const isLoading = ref(true);
-  const loggedUser = ref(null);
-
-  const authenticateUser = async () => {
+// Crea un objeto que actúe como contexto de autenticación
+const authContext = {
+  isLoggedIn: ref(false),
+  isLoading: ref(true),
+  loggedUser: ref(null),
+  authenticateUser: async () => {
     try {
       const response = await service.get("auth/verify");
-      isLoggedIn.value = true;
-      isLoading.value = false;
-      loggedUser.value = response.data.payload;
+      authContext.isLoggedIn.value = true;
+      authContext.isLoading.value = false;
+      authContext.loggedUser.value = response.data.payload;
     } catch (error) {
-      isLoggedIn.value = false;
-      isLoading.value = false;
-      loggedUser.value = null;
+      authContext.isLoggedIn.value = false;
+      authContext.isLoading.value = false;
+      authContext.loggedUser.value = null;
     }
-  };
+  },
+};
 
-  onMounted(() => {
-    authenticateUser();
-  });
-
-  return { authenticateUser, isLoggedIn, loggedUser };
-}
-
-export { AuthContext, useAuth };
+export default authContext;
