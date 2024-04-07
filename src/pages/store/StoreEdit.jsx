ed32cee4a678d30e50fff3b2d5cdbcb5f3db60a7
raulgp <raul.garrigos@hotmail.com>
2024-04-07 11:22:12 +0200

    Added edit store button and edit WIP

--- src/pages/store/StoreEdit.jsx
+++ src/pages/store/StoreEdit.jsx
@@ -1,7 +1,83 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import service from "../../services/config";
 
 function StoreEdit() {
-  return <div>StoreEdit</div>;
+  const [storeData, setStoreData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  const redirect = useNavigate();
+  const params = useParams();
+
+  const handleInputChange = (e) => {
+    const clone = JSON.parse(JSON.stringify(storeData));
+    clone[e.target.name] = e.target.value;
+    setStoreData(clone);
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = async () => {
+    try {
+      const response = await service.get(`/store/${params.storeId}`);
+      setUserData(response.data);
+      setIsLoading(false);
+    } catch (error) {
+      redirect("/error");
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await service.put(`/store/${params.storeId}`, storeData);
+      redirect(`/store/${params.storeId}`);
+    } catch (error) {
+      redirect("/error");
+    }
+  };
+
+  if (isLoading) {
+    return <h3>Loading...</h3>;
+  }
+
+  // Styles
+  const containerStyle = {
+    maxWidth: "600px",
+    backgroundColor: "grey",
+    padding: "20px",
+    borderRadius: "8px",
+  };
+
+  return (
+    <div>
+      <h3>Update your store: </h3>
+      <Container className="text-center" style={containerStyle}>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group controlId="formStoreName">
+            <Form.Label>Store name:</Form.Label>
+            <Form.Control
+              type="text"
+              name="storeName"
+              onChange={handleInputChange}
+              defaultValue={storeData.name}
+            />
+          </Form.Group>
+
+          <Button
+            variant="light"
+            type="submit"
+            style={{ backgroundColor: "#fdb14d" }}
+          >
+            Confirm changes
+          </Button>
+          <br />
+        </Form>
+      </Container>
+    </div>
+  );
 }
 
 export default StoreEdit;
