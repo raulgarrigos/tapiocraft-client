1f0cce219d142b377f4614e768e6728dcf1d5cfd
raulgp <raul.garrigos@hotmail.com>
2024-05-02 13:24:21 +0200

    Basic styles done

--- src/pages/orders/OrderDetails.jsx
+++ src/pages/orders/OrderDetails.jsx
@@ -47,33 +47,35 @@ function OrderDetails() {
     <div className="container my-4 px-4 py-8 bg-white rounded-lg shadow-md">
       {loggedUser && loggedUser._id === orderDetails?.user._id && (
         <div>
-          <div className="mb-4 p-4 bg-gray-100 rounded-lg border-2 border-gray-200">
-            <h4 className="text-3xl font-bold my-2">Datos del pedido</h4>
-            <div>
-              <p>Nº: {orderDetails._id}</p>
-              <p>Estado del pedido: {orderDetails.status}</p>
-              <p>Precio total: {orderDetails.orderPrice}€</p>
+          <div className="lg:flex lg:space-x-4 mb-4">
+            <div className="p-4 bg-gray-100 rounded-lg border-2 border-gray-200 flex-1 my-2">
+              <h4 className="text-3xl font-bold my-2">Datos del pedido</h4>
+              <div>
+                <p>Nº: {orderDetails._id}</p>
+                <p>Estado del pedido: {orderDetails.status}</p>
+                <p>Precio total: {orderDetails.orderPrice}€</p>
+              </div>
             </div>
-          </div>
-          <div className="mb-4 p-4 bg-gray-100 rounded-lg border-2 border-gray-200">
-            <h4 className="text-3xl font-bold">Datos de contacto</h4>
-            <div>
-              <p>
-                Nombre: {orderDetails.name} {orderDetails.surname}
-              </p>
-              <p>Nº de teléfono: {orderDetails.user.phoneNumber}</p>
-              <p>Correo electrónico: {orderDetails.user.email}</p>
-              <p>Dirección de envío: {orderDetails.shippingAddress}</p>
+            <div className="p-4 bg-gray-100 rounded-lg border-2 border-gray-200 flex-1 my-2">
+              <h4 className="text-3xl font-bold">Datos de contacto</h4>
+              <div>
+                <p>
+                  Nombre: {orderDetails.name} {orderDetails.surname}
+                </p>
+                <p>Nº de teléfono: {orderDetails.user.phoneNumber}</p>
+                <p>Correo electrónico: {orderDetails.user.email}</p>
+                <p>Dirección de envío: {orderDetails.shippingAddress}</p>
+              </div>
+              {orderDetails.status !== "cancelled" && (
+                <button
+                  type="button"
+                  onClick={handleCancelOrder}
+                  className="my-forms-deleteButton"
+                >
+                  Cancel order
+                </button>
+              )}
             </div>
-            {orderDetails.status !== "cancelled" && (
-              <button
-                type="button"
-                onClick={handleCancelOrder}
-                className="my-forms-deleteButton"
-              >
-                Cancel order
-              </button>
-            )}
           </div>
           <div className="mb-4 p-4 bg-gray-100 rounded-lg border-2 border-gray-200">
             <h4 className="text-3xl font-bold my-2">Productos</h4>
@@ -81,23 +83,47 @@ function OrderDetails() {
               {orderDetails.products.map((product) => (
                 <div
                   key={product._id}
-                  className=" my-4 px-4 py-8 bg-white rounded-lg shadow-md hover:scale-105 hover:border border-indigo-600"
+                  className=" my-4 px-4 py-auto bg-white rounded-lg shadow-md hover:scale-105 hover:border border-indigo-600"
                 >
                   <Link
                     to={`/store/${product.product.store}/${product.product._id}`}
                   >
-                    <p>Nombre: {product.product.name}</p>
-                    <p>Precio: {product.product.price}€</p>
-                    <p>Cantidad: {product.quantity}</p>
-                    {orderDetails.stores.map((store) => {
-                      return (
-                        <p key={store._id}>
-                          {product.product.store === store._id
-                            ? `Tienda: ${store.name}`
-                            : ""}
-                        </p>
-                      );
-                    })}
+                    <div className="flex items-center mb-auto pt-4">
+                      <div className="mr-4">
+                        {product.product.images &&
+                        product.product.images.length > 0 ? (
+                          <div className="w-40 h-40 max-w-full mx-auto rounded-md overflow-hidden">
+                            <img
+                              src={product.product.images[0]}
+                              alt={`Image ${product.product.images[0]}`}
+                              className="object-contain w-full h-full"
+                            />
+                          </div>
+                        ) : (
+                          <div className="w-40 h-40 max-w-full mx-auto rounded-md overflow-hidden">
+                            <img
+                              src="/public/images/image_1024.png"
+                              alt="Imagen default"
+                              className="object-contain w-full h-full"
+                            />
+                          </div>
+                        )}
+                      </div>
+                      <div className="flex-grow">
+                        <p>Nombre: {product.product.name}</p>
+                        <p>Precio: {product.product.price}€</p>
+                        <p>Cantidad: {product.quantity}</p>
+                        {orderDetails.stores.map((store) => {
+                          return (
+                            <p key={store._id}>
+                              {product.product.store === store._id
+                                ? `Tienda: ${store.name}`
+                                : ""}
+                            </p>
+                          );
+                        })}
+                      </div>
+                    </div>
                   </Link>
                   <br />
                 </div>
