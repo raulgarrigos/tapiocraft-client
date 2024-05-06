b6b4bd420ba81200011d2524c3df25bfbd6d858e
raulgp <raul.garrigos@hotmail.com>
2024-05-06 12:18:41 +0200

    product card

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
--- src/pages/products/ProductDetails.jsx
+++ src/pages/products/ProductDetails.jsx
@@ -2,7 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import { Link, useNavigate, useParams } from "react-router-dom";
 import service from "../../services/config";
 import { AuthContext } from "../../context/auth.context";
-import { Button } from "react-bootstrap";
+import { Carousel } from "react-responsive-carousel";
 
 function ProductDetails() {
   const [productDetails, setProductDetails] = useState(null);
@@ -39,7 +39,7 @@ function ProductDetails() {
   const addToCart = async () => {
     try {
       await service.post(`/cart/products/${params.productId}`);
-      redirect("/cart");
+      getData();
     } catch (error) {
       redirect("/error");
     }
@@ -62,68 +62,80 @@ function ProductDetails() {
   }
 
   return (
-    <div className="container mx-auto px-4 py-8">
-      <h2 className="text-2xl font-semibold mb-4 text-left">
-        {productDetails.name}
-      </h2>
-      <p className="text-lg mb-4 text-left">{productDetails.description}</p>
-      <p className="text-lg mb-4 text-left">Price: {productDetails.price}€</p>
-      <p className="text-lg mb-4 text-left">
-        Stock: {productDetails.stock} unit/s
-      </p>
-
-      <div className="flex flex-wrap gap-4 mb-8">
-        {productDetails.images.map((image, index) => (
-          <div key={index} className="relative w-40">
-            <img
-              src={image}
-              alt={`Image ${index}`}
-              className="w-full rounded"
-            />
-            {loggedUser && loggedUser._id === storeDetails?.owner && (
-              <button
-                className="absolute top-0 right-0 p-1 bg-red-500 text-white rounded text-sm"
-                onClick={() => handleDeleteImage(image)}
-              >
-                Delete
-              </button>
-            )}
-          </div>
-        ))}
+    <div className="flex font-sans bg-white rounded-lg shadow-md mt-3 py-3 px-3">
+      <div className="flex-none w-56 relative">
+        <Carousel showArrows={true} showStatus={false} showIndicators={false}>
+          {productDetails.images.map((image, index) => (
+            <div key={index}>
+              <img
+                src={image}
+                alt={`Image ${index}`}
+                className="w-full h-full object-cover rounded-lg"
+              />
+              {loggedUser && loggedUser._id === storeDetails?.owner && (
+                <button
+                  className="absolute top-0 right-0 p-1 bg-red-500 text-white rounded text-sm"
+                  onClick={() => handleDeleteImage(image)}
+                >
+                  Delete
+                </button>
+              )}
+            </div>
+          ))}
+        </Carousel>
       </div>
-
-      {loggedUser && loggedUser._id !== storeDetails?.owner && (
-        <Button
-          variant="primary"
-          onClick={addToCart}
-          disabled={productDetails.stock === 0}
-        >
-          Add to Cart
-        </Button>
-      )}
-
-      {loggedUser && loggedUser._id === storeDetails?.owner && (
-        <div className="mt-4">
-          <Link
-            to={`/store/${params.storeId}/${productDetails._id}/edit`}
-            className="mr-2 inline-block bg-purple-500 hover:bg-purple-600 text-white py-2 px-4 rounded-lg"
-          >
-            <button className="focus:outline-none">Edit Product</button>
-          </Link>
-          <Link
-            to={`/store/${params.storeId}/${productDetails._id}/image`}
-            className="mr-2 inline-block bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded-lg"
-          >
-            <button className="focus:outline-none">Add Image</button>
-          </Link>
-          <Link
-            to={`/store/${params.storeId}`}
-            className="inline-block bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded-lg"
-          >
-            <button className="focus:outline-none">Back</button>
-          </Link>
+      <form className="flex-auto p-6">
+        <div className="flex flex-wrap">
+          <h1 className="flex justify-start font-medium mb-3 text-slate-900 text-2xl">
+            {productDetails.name}
+          </h1>
+          <div className="flex-auto text-sm font-medium text-slate-400 text-right">
+            {productDetails.stock > 1 ? "In stock" : "Out of stock"}
+          </div>
         </div>
-      )}
+        <div className=" flex justify-start w-full text-3xl mb-3 font-bold text-violet-600">
+          {productDetails.price}€
+        </div>
+        <div className="flex justify-start font-medium mb-3 text-slate-900 text-lg">
+          <h3>Description</h3>
+        </div>
+        <div className="flex justify-start font-medium mb-3 text-slate-600 text-base">
+          <p>{productDetails.description}</p>
+        </div>
+        <div className="flex items-baseline mt-4 mb-6 pb-6 border-b border-slate-200"></div>
+        <div className="flex space-x-4 mb-5 text-sm font-medium">
+          {(!loggedUser ||
+            (loggedUser && loggedUser._id !== storeDetails?.owner)) && (
+            <button
+              className="h-10 px-6 font-semibold rounded-full border border-slate-200 text-slate-900"
+              type="button"
+              onClick={addToCart}
+              disabled={productDetails.stock === 0}
+            >
+              Add to bag
+            </button>
+          )}
+          {loggedUser && loggedUser._id === storeDetails?.owner && (
+            <>
+              <Link
+                to={`/store/${params.storeId}/${productDetails._id}/edit`}
+                className="flex-auto flex items-center justify-center space-x-4 h-10 px-6 font-semibold rounded-full bg-yellow-600 text-white"
+              >
+                <button className="focus:outline-none">Edit Product</button>
+              </Link>
+              <Link
+                to={`/store/${params.storeId}/${productDetails._id}/image`}
+                className="flex-auto flex  items-center justify-center space-x-4 h-10 px-6 font-semibold rounded-full bg-violet-600 text-white"
+              >
+                <button className="focus:outline-none">Add Image</button>
+              </Link>
+            </>
+          )}
+        </div>
+        <p class="flex justify-start text-sm text-slate-500">
+          Free shipping on all european orders.
+        </p>
+      </form>
     </div>
   );
 }
